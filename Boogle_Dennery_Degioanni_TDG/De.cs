0d80093277472fa2d6686e739209a39cdd9055e7
LoXaAa <hugo.degioanni@gmail.com>
2024-11-15 09:15:55 +0100

    Création des classe

---
+++ Boogle_Dennery_Degioanni_TDG/De.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boogle_Dennery_Degioanni_TDG
+{
+    internal class De
+    {
+    }
+}
