62bc35178be32519ed0b4d1486c5f89b2800af3a
LoXaAa <hugo.degioanni@gmail.com>
2024-11-15 08:26:29 +0100

    Ajoutez des fichiers projet.

---
+++ Boogle_Dennery_Degioanni_TDG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Boogle_Dennery_Degioanni_TDG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
